78951b8672f5ee20603a1216bf99ce23767af3e6
Teol55 <m.ch5500@gmail.com>
2019-07-22 10:07:03 +0200

    Start form

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -2,14 +2,15 @@
 
 namespace App\Controller;
 
-use App\Entity\Request;
+use App\Entity\Article;
+
 use App\Form\ArticleFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
 class ArticleAdminController extends AbstractController
 {
-
 }
--- src/Controller/ComiteAdminController.php
+++ src/Controller/ComiteAdminController.php
@@ -3,10 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Article;
-use App\Entity\Request;
+
 use App\Form\ArticleFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ComiteAdminController extends AbstractController
@@ -15,12 +16,27 @@ class ComiteAdminController extends AbstractController
      * @Route("/article/new", name="admin_article_new")
      * IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em,Request $request)
     {
 
         $form = $this->createForm(ArticleFormType::class);
 
-        return $this->render('article_admin/admin.html.twig', [
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            /** @var Article $article */
+            $article=$form->getData();
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success','le document a bien été enregistré');
+            return $this->redirectToRoute('app_homepage');
+
+        }
+
+        return $this->render('article_admin/new.html.twig', [
             'adminForm' => $form->createView()
         ]);
     }
