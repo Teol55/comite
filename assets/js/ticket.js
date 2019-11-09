7bcd20d3c795bf7821ceebc275580ad1d040400b
C_MARTIN <m.ch55000@gmail.com>
2019-11-09 12:52:19 +0100

    change style for tickets

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,30 +1,38 @@
 
 tickets=document.getElementsByClassName('ticket__offer');
 rowSelectedElts=document.getElementsByClassName('')
+function close(){
+    divsCard=document.getElementsByClassName('ticket__offer__card');
+    divsCard.forEach(function (div){
 
+        div.style.display='none';
+
+    })
+    triangleElts=document.getElementsByClassName('ticket__offer__triangle');
+    triangleElts.forEach(function (triangle) {
+        triangle.style.opacity=0;
+
+    })
+
+    // delete the row seleceted by an other clic
+    var rowSelecetedElts=document.getElementsByClassName('test__row--selected');
+    rowSelecetedElts.forEach(function(row){
+        row.classList.remove("test__row--selected");
+    })
+
+    // delete the offer activate by an other clic
+
+    var offerActiveElts=document.getElementsByClassName('test__offer--active');
+    offerActiveElts.forEach(function (offer) {
+        offer.classList.remove('test__offer--active');
+
+    })
+}
 tickets.forEach(function (ticket) {
         ticket.addEventListener('click',function  (e){console.log('j ai cliqué');
 
-            divsCard=document.getElementsByClassName('ticket__offer__card');
-            divsCard.forEach(function (div){
+        close();
 
-                div.style.display='none';
-
-            });
-
-        // delete the row seleceted by an other clic
-        var rowSelecetedElts=document.getElementsByClassName('test__row--selected');
-            rowSelecetedElts.forEach(function(row){
-            row.classList.remove("test__row--selected");
-        })
-
-            // delete the offer activate by an other clic
-
-        var offerActiveElts=document.getElementsByClassName('test__offer--active');
-            offerActiveElts.forEach(function (offer) {
-                offer.classList.remove('test__offer--active');
-
-            })
 
         // add class test__row__selected at the row who offer was actived
         var rowOfferElt=e.target.closest(".row");
@@ -32,9 +40,18 @@ tickets.forEach(function (ticket) {
 
         // add class offer--active for check the offer was clic
         var offerActiveElt=e.target.closest('.ticket__offer__container');
-        offerActiveElt.classList.add('test__offer--active');
+        offerActiveElt.classList.add('test__offer--active')
+            // show the card element with triangle
         var cardElt=offerActiveElt.querySelector('.ticket__offer__card');
+        var triangleElt=offerActiveElt.querySelector('.ticket__offer__triangle');
         cardElt.style.display='block';
+        triangleElt.style.opacity=1;
+
+            closeElt=offerActiveElt.querySelector('.ticket__cardBox__close');
+
+            closeElt.addEventListener('click',close);
+
+
     });
 
 
